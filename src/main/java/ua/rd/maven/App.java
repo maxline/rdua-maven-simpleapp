0ad2c2f5b27063b659144f9a4f511c74b9a909de
maxline <sergml@mail.ru>
2016-10-07 17:43:38 +0300

    first commit

---
+++ src/main/java/ua/rd/maven/App.java
@@ -0,0 +1,13 @@
+package ua.rd.maven;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World1! ");
+        for(int i=0; i< args.length; i++){
+            System.out.println("param = " + args[i]);
+        }
+    }
+}
---
+++ src/test/java/ua/rd/maven/AppTest.java
@@ -0,0 +1,15 @@
+package ua.rd.maven;
+
+import org.junit.Test;
+
+public class AppTest  {
+
+    @Test
+    public void main() throws Exception {
+        String[] str = {"param1", "param2"};
+
+
+        App.main(str);
+    }
+
+}
