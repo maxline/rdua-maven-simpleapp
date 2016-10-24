208e2259b95fd11c936c1ef2b209d2322a484801
maxline <sergml@mail.ru>
2016-10-24 16:40:11 +0300

    Task 1. finished

--- src/main/java/ua/rd/maven/App.java
+++ src/main/java/ua/rd/maven/App.java
@@ -7,7 +7,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World1! ");
         for(int i=0; i< args.length; i++){
-            System.out.println("param = " + args[i]);
+            System.out.println("param[" +i+"] = "  + args[i]);
         }
     }
 }
